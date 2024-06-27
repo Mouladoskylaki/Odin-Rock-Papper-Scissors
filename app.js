cf66bc5ecdce7e7b579d6f63f977bbc7ada41630
Konstantinos Adamopoulos <adamssorama@gmail.com>
2024-06-27 22:26:39 +0300

    Add getHumanChoice and playRound functions for Rock Paper Scissors
    - Implemented getHumanChoice function to prompt
    user input and ensure valid choices (rock, paper,
    or scissors).
    - Added case-insensitive input to
    getHumanChoice function.
    - Implemented playRound function to determine the
    winner between human and computer.
    - Updated playRound function to correctly handle
    tie cases and increment scores based on game rules
    
    These functions now properly handle user input and
    game logic for a single round.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const choices = ["rock", "papper", "scissors"];
+const choices = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let humanScore = 0;
 
 const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * choices.length);
@@ -10,4 +12,47 @@ const getComputerChoice = () => {
     return computerChoice;
 }
 
-console.log(getComputerChoice())
+// console.log(getComputerChoice());
+
+const getHumanChoice = () => {
+    let humanChoice = prompt("Rock, Paper, Scissors");
+
+    humanChoice = humanChoice.toLowerCase();
+
+    while (!choices.includes(humanChoice)) {
+        humanChoice = prompt("Invalid selection! Choose between Rock, Paper or Scissors");
+    }
+    console.log(humanChoice);
+    return humanChoice;
+    
+}
+
+// getHumanChoice()
+
+const playRound = (humanChoice, computerChoice) => {
+      
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "paper" ||
+        humanChoice === "paper" && computerChoice === "rock"
+    ) {
+        humanScore++;
+        console.log("You win this round!")
+    } else {
+        computerScore++;
+        console.log("Computer wins this round");
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log("Human Choice: " + humanSelection);
+console.log("Computer Choice: " + computerSelection);
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: " + computerScore);
