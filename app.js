9b2fc59c0940172286a30c10f9b9dfeabd86363c
Konstantinos Adamopoulos <adamssorama@gmail.com>
2024-06-28 23:02:02 +0300

    Add initial implementation of playGame function
    - Included getHumanChoice and getComputerChoice to
    capture player and computer choices
    - Implemented playRound function to determine the
    winner of a round
    - Added score tracking and logging of results
    
    Note: The playGame function is not yet complete
    and requires further enhancements

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const choices = ["rock", "paper", "scissors"];
-let computerScore = 0;
-let humanScore = 0;
 
 const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * choices.length);
@@ -13,46 +11,71 @@ const getComputerChoice = () => {
 }
 
 // console.log(getComputerChoice());
+let humanChoice = prompt("Rock, Paper, Scissors");
 
 const getHumanChoice = () => {
-    let humanChoice = prompt("Rock, Paper, Scissors");
+    
 
     humanChoice = humanChoice.toLowerCase();
 
     while (!choices.includes(humanChoice)) {
         humanChoice = prompt("Invalid selection! Choose between Rock, Paper or Scissors");
     }
-    console.log(humanChoice);
     return humanChoice;
     
 }
 
 // getHumanChoice()
 
-const playRound = (humanChoice, computerChoice) => {
+let computerScore = 0;
+let humanScore = 0;
+
+const playGame = () => {
+
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+ 
+    const playRound = (humanChoice, computerChoice) => {
       
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!")
-    } else if (
-        humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "scissors" && computerChoice === "paper" ||
-        humanChoice === "paper" && computerChoice === "rock"
-    ) {
-        humanScore++;
-        console.log("You win this round!")
-    } else {
-        computerScore++;
-        console.log("Computer wins this round");
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie!")
+        } else if (
+            humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "paper" ||
+            humanChoice === "paper" && computerChoice === "rock"
+        ) {
+            humanScore++;
+            console.log("You win this round!")
+        } else {
+            computerScore++;
+            console.log("Computer wins this round");
+        }
+    
+    }
+    
+    playRound(humanSelection, computerSelection)
+
+    console.log("Human Choice: " + humanSelection);
+    console.log("Computer Choice: " + computerSelection);
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+
+    while (humanScore < 5) {
+        humanChoice = prompt(`Human Score: ${humanScore} - Computer Score: ${computerScore}
+            Next Round`)
+        playGame()
+    }
+    
+    if (humanScore === 5) {
+        console.log("You Win!")
+    }
+    
+    if (computerScore === 5) {
+        console.log("You Loose!")
     }
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+playGame();
 
-playRound(humanSelection, computerSelection);
-
-console.log("Human Choice: " + humanSelection);
-console.log("Computer Choice: " + computerSelection);
-console.log("Human Score: " + humanScore);
-console.log("Computer Score: " + computerScore);
